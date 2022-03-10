6b2ef4d55538fbc902059a6ac0ff41479f9fd682
NahuelCF <cristiannahuel95@gmail.com>
2022-03-10 19:07:14 -0300

    Adding atributes to Home class

--- ecommerce-backend/src/main/java/com/ecommerce/domain/strategy/Home.java
+++ ecommerce-backend/src/main/java/com/ecommerce/domain/strategy/Home.java
@@ -1,17 +1,37 @@
 package com.ecommerce.domain.strategy;
 
+import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.MapKeyColumn;
+import java.util.HashMap;
 import java.util.Map;
 
 @Entity
 @DiscriminatorValue("Home")
 public class Home extends Category {
+
+    private Integer day;
+    private Integer from;
+    private Integer to;
+    private Double discount;
+
+    public Home(Integer day,Integer from,Integer to,Double discount){
+        this.day = day;
+        this.from = from;
+        this.to = to;
+        this.discount = discount;
+    }
+
+    public Home() {
+
+    }
+
     @Override
     public Double discount(Double price) {
         Map<String, Integer> dayAndTime = Utils.getDayAndTime();
         if(this.correctDayAndTime(dayAndTime)){
-            return price * 0.8;
+            return price * discount;
         } else{
             return price;
         }
@@ -19,10 +39,6 @@ public class Home extends Category {
 
     @Override
     public Boolean correctDayAndTime(Map<String, Integer> dayAndTime) {
-        //TODO: hacer el dia y hora un atributo
-        System.out.print("HOME");
-        System.out.print("DAY: " + dayAndTime.get("day").equals(2));
-        System.out.print("TIME: " + Utils.between(dayAndTime.get("time"),20,22));
-        return dayAndTime.get("day").equals(2) && Utils.between(dayAndTime.get("time"),20,22);
+        return dayAndTime.get("day").equals(day) && Utils.between(dayAndTime.get("time"),from,to);
     }
 }
